d82f85aad5d81b8fa1e8ce280749fc5d9d36e46d
chandra <chandrasaimohan.bhupathi@cognizant.com>
2020-05-24 21:26:51 +0530

    Made changs in master on the sameline made by Very-bad branch

--- app/src/main/java/com/git/practice/GitCommads.kt
+++ app/src/main/java/com/git/practice/GitCommads.kt
@@ -163,5 +163,5 @@ Merge changes from “updates” branch to “master” branch.
 
 (30) To Delete a branch:
 git branch -d updates
-git branch -a : Displays list of all branches. Made Changes on the same line very-bad branch made from master
+git branch -a : Displays list of all branches. Made changes from master on the same line made by very-bad.
 */
